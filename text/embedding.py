b6bceacf5761550fbefdaeafdb60da479a48903b
danial.moafi <danial.moafi@snapp.cab>
2025-01-01 19:27:11 +0100

    remove extra files

--- text/embedding.py
+++
@@ -1,6 +0,0 @@
-from model2vec import StaticModel
-
-model = StaticModel.from_pretrained("minishlab/potion-base-4M")
-embeddings = model.encode_as_sequence(["Example sentence"])
-
-print(embeddings)
