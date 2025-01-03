930954f3573cc1a4866028b6b3febbfbda1b2aa7
danial.moafi <danial.moafi@snapp.cab>
2025-01-03 12:42:11 +0100

    fix bugs: related to paths

--- scripts/prepare_data.sh
+++ scripts/prepare_data.sh
@@ -4,17 +4,19 @@
 set -e
 
 # Data path
-RAW_DATA_DIR="../data/raw"
-PROCESSED_DATA_DIR="../data/processed"
+RAW_DATA_DIR="data/raw"
+PROCESSED_DATA_DIR="data/processed"
 
 echo "🚀 Starting data preparation..."
 
 # Activate the virtual environment
-source ../.venv/bin/activate
+source .venv/bin/activate
 
 # Install unzip
-echo "📥 Installing unzip..."
-sudo apt-get install unzip
+if ! command -v unzip &> /dev/null; then
+    echo "📥 Installing unzip..."
+    sudo apt-get install unzip
+fi
 
 # Download dataset 
 if [ ! -d "$RAW_DATA_DIR" ]; then
@@ -26,47 +28,44 @@ if [ ! -d "$RAW_DATA_DIR" ]; then
     if [ "$dataset" == "1" ]; then
         curl -L -o $RAW_DATA_DIR/flickr-image-dataset.zip\
             https://www.kaggle.com/api/v1/datasets/download/adityajn105/flickr8k
+        
+        echo "📂 Unzipping dataset..."
+        unzip -q $RAW_DATA_DIR/flickr-image-dataset.zip -d $RAW_DATA_DIR
+        mv $RAW_DATA_DIR/Images $RAW_DATA_DIR/images
+
     elif [ "$dataset" == "2" ]; then
         curl -L -o $RAW_DATA_DIR/flickr-image-dataset.zip\
             https://www.kaggle.com/api/v1/datasets/download/hsankesara/flickr-image-dataset
+        
+        echo "📂 Unzipping dataset..."
+        unzip -q $RAW_DATA_DIR/flickr-image-dataset.zip -d $RAW_DATA_DIR
+        mkdir -p $RAW_DATA_DIR/images
+        mv $RAW_DATA_DIR/flickr-image-dataset/*.jpg $RAW_DATA_DIR/images/
+        mv $RAW_DATA_DIR/flickr-image-dataset/results.csv $RAW_DATA_DIR/captions.txt
     else
         echo "Invalid dataset selection. Please select either flickr8k or flickr30k."
         exit 1
     fi
     
-    # Unzip the dataset
-    echo "📂 Unzipping dataset..."
-    unzip -q $RAW_DATA_DIR/flickr-image-dataset.zip -d $RAW_DATA_DIR
-    mkdir -p $RAW_DATA_DIR/images
-    mv $RAW_DATA_DIR/flickr30k_images/*.jpg $RAW_DATA_DIR/images/
-    mv $RAW_DATA_DIR/flickr30k_images/results.csv $RAW_DATA_DIR/captions.txt
 
     # Remove the zip file
     echo "📂 Removing zip file..."
     rm $RAW_DATA_DIR/flickr-image-dataset.zip
 fi
 
-# Check if dataset exists
+# Check if processed data exists
 if [ -d "$PROCESSED_DATA_DIR" ]; then
-    echo "⚠️  Dataset already exists!"
+    echo "⚠️  Processed dataset already exists!"
     while true; do
-        read -p "Do you want to delete existing data and download again? (y/n): " yn
+        read -p "Do you want to delete existing processed data and create a new one? (y/n): " yn
         case $yn in
             [Yy]* )
-                echo "🗑️  Removing existing dataset..."
+                echo "🗑️  Removing existing processed dataset..."
                 rm -rf "$PROCESSED_DATA_DIR"
-                mkdir -p $PROCESSED_DATA_DIR
-
-                # Run the pre_processing.py script
-                echo "🔑 Preprocessing dataset..."
-                python data/pre_processing.py
-                echo "🔑 Tokenizing dataset..."
-                python text/tokenizer.py
-
                 break
                 ;;
             [Nn]* )
-                echo "⏭️  Skipping download, using existing dataset..."
+                echo "⏭️  Skipping preprocessing, using existing dataset..."
                 exit 0
                 ;;
             * )
@@ -74,6 +73,15 @@ if [ -d "$PROCESSED_DATA_DIR" ]; then
                 ;;
         esac
     done
+fi
+
+if [ ! -d "$PROCESSED_DATA_DIR" ]; then
+    # Create processed data directory and run preprocessing
+    mkdir -p $PROCESSED_DATA_DIR
+    echo "🔑 Preprocessing dataset..."
+    python data/pre_processing.py
+    echo "🔑 Tokenizing dataset..."
+    python text/tokenizer.py
 fi
 
 echo "✅ Data preparation completed successfully!"
